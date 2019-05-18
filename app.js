db5adc0b3699cc5d713c5147c3b8fa38dbf16609
terrirp <terri.persad@gmail.com>
2019-05-18 10:13:58 -0400

    working on matching cards

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 const flipCardArray = [
     {
         title: "happy frog",
-        class: "happy frog",
+        class: "happyFrog",
         img: "./assets/frogSmile.svg"
         
     },
      {
         title: "sad frog",
-        class: "sad frog",
+        class: "sadFrog",
         img: "./assets/frogSad.svg"
     },
      {
@@ -18,29 +18,29 @@ const flipCardArray = [
     },
     {
         title: "girl frog sad",
-        class: "girl frog sad",
+        class: "girlFrogSad",
         img: "./assets/girlFrogSad.svg"
     },
     {
         title: "girl frog happy",
-        class: "girl frog happy",
+        class: "girlFrogHappy",
         img: "./assets/girlFrogSmile.svg"
 
     },
     {
         title: "girl frog shocked",
-        class: "girl frog shocked",
+        class: "girlFrogShocked",
         img: "./assets/girlFrogShocked.svg"
     },
       {
         title: "happy frog",
-        class: "happy frog",
+        class: "happyFrog",
         img: "./assets/frogSmile.svg"
         
     },
      {
         title: "sad frog",
-        class: "sad frog",
+        class: "sadFrog",
         img: "./assets/frogSad.svg"
     },
      {
@@ -50,18 +50,18 @@ const flipCardArray = [
     },
     {
         title: "girl frog sad",
-        class: "girl frog sad",
+        class: "girlFrogSad",
         img: "./assets/girlFrogSad.svg"
     },
     {
         title: "girl frog happy",
-        class: "girl frog happy",
+        class: "girlFrogHappy",
         img: "./assets/girlFrogSmile.svg"
 
     },
     {
         title: "girl frog shocked",
-        class: "girl frog shocked",
+        class: "girlFrogShocked",
         img: "./assets/girlFrogShocked.svg"
     }
 ]
@@ -70,18 +70,18 @@ const $memoryGame = $('#memory');
 const $game = $("<section>").attr('class', 'game');
 $memoryGame.append($game);
 
-
-    
+   
 $(function(){
+
 // //shuffle cards each time page reloads
 function shuffle(array) { //this code is from stack overflow
-  let currentIndex = array.length,
-    temporaryValue,
-    randomIndex;
- 
-  // While there remain elements to shuffle...
+  let currentIndex = array.length;
+  let temporaryValue;
+   let randomIndex;
+
   while (0 !== currentIndex) {
     // Pick a remaining element...
+
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
 
@@ -97,19 +97,52 @@ const shuffledCards = shuffle(flipCardArray); //calling shuffle of flipCardArray
 
 //this prints the cards to the screen 
 shuffledCards.forEach(item => {
-  const $flip = $("<div>").attr("class", "card");
+  const $card = $("<div>").attr("class", "card");
+  // const $front = $("<div>").attr("class", "front");
+  const $flip = $("<div>").attr("class", "back").attr("class", item.class);
   const $image = $("<img>")
     .attr("src", item.img)
     .attr("class", "frog");
+
+  // $card.append($front)
+    $card.append($flip)
   $flip.append($image);
-  $game.append($flip);
+  $game.append($card);
+
 });
+
+  let firstClick = ''
+  let secondClick = ''
+  let count = 0
+
 $(".card").on("click", function() {
-    console.log('clicked');
+  let clicked = this;
+  // console.log(this);
+    if (clicked.element === "section" || clicked.class === "selected") {
+  return;
+} 
+// clicked.classList.add('selected');
 
+  if (count < 2) {
+    count++;
 
+    if(count === 1);
+    firstClick = clicked.card.div.class;
+    console.log(firstClick);
+
+    clicked.classList.add('selectedCard') //added class so only two cards will be "selected"
+    //when I use .addClass it does not work. 
+  }
+
+  //class cardMatch will make the selected cards disapper if there is a match. Function cardMatch will be called above in the click event. 
+  const cardMatch = () => {
+    let $selectedCard = $('.selectedCard')
     
+    $selectedCard.forEach(card => {
+      card.classList.add('cardMatch')
+    })
+  }
+  
 })
-
 
 })
